358efe6dbe237d8ac665c09f83a37fd87265f787
AntipinVlad <antipinmail1@gmail.com>
2024-07-04 21:24:25 +0300

    init commit

---
+++ src/main/java/org/example/javaproglovo/JavaProGlovoApplication.java
@@ -0,0 +1,13 @@
+package org.example.javaproglovo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProGlovoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaProGlovoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/javaproglovo/JavaProGlovoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.javaproglovo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProGlovoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
